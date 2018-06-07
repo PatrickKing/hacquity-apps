2595bb371caa226e8340fa5484f8e9cc5118dd04
Patrick King <patrick.f.king@gmail.com>
2018-06-07 12:40:07 -0600

    Add UserConnection, add some attributes to User.

--- db/migrate/20180607180116_create_service_postings.rb
+++ db/migrate/20180607180116_create_service_postings.rb
@@ -1,10 +1,10 @@
 class CreateServicePostings < ActiveRecord::Migration[5.2]
   def change
     create_table :service_postings do |t|
-      t.string :postingType
+      t.string :posting_type
       t.string :summary
       t.string :description
-      t.decimal :fullTimeEquivalents
+      t.decimal :full_time_equivalents
       t.references :user, foreign_key: true
       t.boolean :closed
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_215919) do
+ActiveRecord::Schema.define(version: 2018_06_07_181909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "service_postings", force: :cascade do |t|
+    t.string "posting_type"
+    t.string "summary"
+    t.string "description"
+    t.decimal "full_time_equivalents"
+    t.bigint "user_id"
+    t.boolean "closed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_service_postings_on_user_id"
+  end
+
+  create_table "user_connections", force: :cascade do |t|
+    t.bigint "initiator_id"
+    t.bigint "receiver_id"
+    t.string "connection_type"
+    t.bigint "initiator_posting_id"
+    t.bigint "receiver_posting_id"
+    t.boolean "accepted"
+    t.boolean "declined"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["initiator_id"], name: "index_user_connections_on_initiator_id"
+    t.index ["initiator_posting_id"], name: "index_user_connections_on_initiator_posting_id"
+    t.index ["receiver_id"], name: "index_user_connections_on_receiver_id"
+    t.index ["receiver_posting_id"], name: "index_user_connections_on_receiver_posting_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -33,4 +61,9 @@ ActiveRecord::Schema.define(version: 2018_06_04_215919) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "service_postings", "users"
+  add_foreign_key "user_connections", "service_postings", column: "initiator_posting_id"
+  add_foreign_key "user_connections", "service_postings", column: "receiver_posting_id"
+  add_foreign_key "user_connections", "users", column: "initiator_id"
+  add_foreign_key "user_connections", "users", column: "receiver_id"
 end
