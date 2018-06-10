b2111a19f82af4f6201543c9c255a988c67c0de7
Patrick King <patrick.f.king@gmail.com>
2018-06-10 15:36:13 -0600

    Second shift service posting form, WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def nav_link_class(controller, action)
     if controller_name == controller and action_name == action
       'active'
@@ -8,13 +9,11 @@ module ApplicationHelper
   end
 
   def new_create_nav_link_class(controller)
-
     if controller_name == controller and (action_name == 'new' or action_name == 'create')
       'active'
     else
       ''
     end
-
-    
   end
+    
 end
