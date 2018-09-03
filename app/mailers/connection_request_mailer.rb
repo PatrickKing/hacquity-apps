cd3cef9ee6b226414d956036c5db6e0ddf5b3f76
Patrick King <patrick.f.king@gmail.com>
2018-09-03 17:57:54 -0600

    Remove the simpler mailer tutorial stuff. Add a simple throttle on how many times a person can be spammed to connect, withdrawing and creating requests repeatedly should result in only one mail.

--- app/mailers/connection_request_mailer.rb
+++ app/mailers/connection_request_mailer.rb
@@ -3,6 +3,13 @@ class ConnectionRequestMailer < ApplicationMailer
   def connection_requested (connection_request)
     @connection_request = connection_request
 
+    # If this user has sent a connection request to the recipient in the last week, we skip this email to prevent spamming them.
+    recent_connection_requests = ConnectionRequest.where(initiator_id: connection_request.initiator.id,
+        receiver_id: connection_request.receiver.id)
+      .where("created_at > :seven_days_ago", seven_days_ago: 7.days.ago)
+      .where("id <> :id", id: connection_request.id)
+    return if recent_connection_requests.any?
+
     case connection_request.connection_type
     when 'second_shift'
       @connections_url = ss_connection_requests_url
