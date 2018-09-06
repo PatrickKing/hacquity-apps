f289f109b669e3f94410bc30d11c74806b8136f4
Patrick King <patrick.f.king@gmail.com>
2018-09-05 21:10:41 -0600

    Try to turn off activestorage, not sure if it works.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+// activestorage disabled:
+// require activestorage
 //
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
 //= require jquery
 //= require_tree .
